1d02bccce5e48b2b73cdda59aa50c1aa588f4a09
loc phan <phanhoangloc2017@gmail.com>
2021-03-23 21:58:30 +0700

    xay dung layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('login',function(){
+    return view('admin.login');
+});
+
+Route::group(['prefix'=>'admin'],function(){
+
+    
+   
+});
